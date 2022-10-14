ddc6e47b3512be53f21d6f5c25c58d9e9c977295
Gus700 <gustavocruz700@gmail.com>
2022-10-13 20:14:03 -0700

    changed name of timer function

--- matchMaster/main.js
+++ matchMaster/main.js
@@ -22,14 +22,14 @@ function update() {
         displayedNum = floor(rnd(10, 20));
     }
     // if 5 seconds have passed, display "displayNum" for half a second
-    if (displayTextEvery(ticks, 300, 30) == true) {
+    if (resetingTimer(ticks, 300, 30) == true) {
         console.log(displayedNum.toString());
         text(displayedNum.toString(), 50, 50);
     }
 }
 
 // If called inside of update, returns true every n number of ticks for n ticks 
-function displayTextEvery(currentTicks, every_n_ticks, for_n_ticks){
+function resetingTimer(currentTicks, every_n_ticks, for_n_ticks){
     if (((currentTicks % every_n_ticks) >= 0) && (((currentTicks % every_n_ticks) <= for_n_ticks)) ) {
         console.log(every_n_ticks/60, "seconds have passed singal to display text");
         //text(text, x, y);
