df5e8664024c4d44e6fb0cee56a7e74c8eab8084
Gus700 <gustavocruz700@gmail.com>
2022-10-13 20:24:16 -0700

    numbers now change every 5 seconds

--- matchMaster/main.js
+++ matchMaster/main.js
@@ -16,24 +16,26 @@ options = {
     viewSize: {x: G.WIDTH, y: G.LENGTH}
 };
 
-let displayedNum = 0;
+let numToDisplay = 0;
 
 function update() {
     if (!ticks) {
-        displayedNum = floor(rnd(10, 20));
+        numToDisplay = floor(rnd(10, 20));
+    }
+    // if 5 seconds have passed, change numToDisplay
+    if (resetingTimer(ticks, 300, 1) == true) {
+        numToDisplay = floor(rnd(5, 15));
     }
     // if 5 seconds have passed, display "displayNum" for half a second
     if (resetingTimer(ticks, 300, 30) == true) {
-        console.log(displayedNum.toString());
-        text(displayedNum.toString(), 50, 50);
+        text(numToDisplay.toString(), 50, 50);
     }
 }
 
 // If called inside of update, returns true every n number of ticks for n ticks 
 function resetingTimer(currentTicks, every_n_ticks, for_n_ticks){
     if (((currentTicks % every_n_ticks) >= 0) && (((currentTicks % every_n_ticks) <= for_n_ticks)) ) {
-        console.log(every_n_ticks/60, "seconds have passed singal to display text");
-        //text(text, x, y);
+        //console.log(every_n_ticks/60, "seconds have passed singal to display text");
         return true;
     } else {
         return false;
