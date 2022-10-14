390b6746307c80a9013175bb71a2ea25111b2252
Gus700 <gustavocruz700@gmail.com>
2022-10-13 19:31:10 -0700

    added function to signal if n seconds have passed

--- matchMaster/main.js
+++ matchMaster/main.js
@@ -1,13 +1,40 @@
 title = "Match Master";
 
-description = `ye
+description = `
 `;
 
 characters = [];
 
-options = {};
+const G = {
+    WIDTH: 100,
+    LENGTH: 100
+
+};
+
+options = {
+    viewSize: {x: G.WIDTH, y: G.LENGTH}
+};
+
+let displayedNum = 0;
 
 function update() {
-  if (!ticks) {
-  }
+    if (!ticks) {
+        displayedNum = floor(rnd(10, 20));
+    }
+    // if 5 seconds have passed, display "displayNum" for half a second
+    if (displayTextEvery(ticks, 300, 30) == true) {
+        console.log(displayedNum.toString());
+        text(displayedNum.toString(), 50, 50);
+    }
+}
+
+// If called inside of update, returns true every n number of ticks for n ticks 
+function displayTextEvery(currentTicks, every_n_ticks, for_n_ticks){
+    if (((currentTicks % every_n_ticks) >= 0) && (((currentTicks % every_n_ticks) <= for_n_ticks)) ) {
+        console.log(every_n_ticks/60, "seconds have passed singal to display text");
+        //text(text, x, y);
+        return true;
+    } else {
+        return false;
+    }
 }
