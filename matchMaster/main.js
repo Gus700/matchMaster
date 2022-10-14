80143558e68787783f36769c6cc06effccf7c690
Gus700 <gustavocruz700@gmail.com>
2022-10-13 21:21:18 -0700

    added basic clicking logic

--- matchMaster/main.js
+++ matchMaster/main.js
@@ -4,7 +4,21 @@ description = `Click the correct
 amount
 `;
 
-characters = [];
+characters = [
+`
+gg  gg
+gg  gg
+
+gggggg
+ g  g
+  gg
+`,`
+rr  rr
+rr  rr
+
+rrrrrr
+`
+];
 
 const G = {
     WIDTH: 150,
@@ -18,18 +32,28 @@ options = {
 };
 
 let numToDisplay = 0;
-
+let amountCliked = 0;
+let currentlyDisplayingNum = false;
 function update() {
-    if (!ticks) {
+    if (!ticks) { 
         numToDisplay = floor(rnd(10, 20));
     }
-    // if 5 seconds have passed, change numToDisplay
+    currentlyDisplayingNum = false;
+    // if 5 seconds have passed, change numToDisplay and reset amountClicked
     if (resetingTimer(ticks, 300, 1) == true) {
         numToDisplay = floor(rnd(5, 15));
+        amountCliked = 0;
     }
     // if 5 seconds have passed, display "displayNum" for half a second
     if (resetingTimer(ticks, 300, 30) == true) {
+        currentlyDisplayingNum = true;
         text(numToDisplay.toString(), G.WIDTH * 0.5, G.LENGTH * 0.5);
+    }
+    if (input.isJustPressed && amountCliked < numToDisplay && currentlyDisplayingNum == false){
+        amountCliked++;
+        char("a", rnd(0, G.WIDTH), rnd(0, G.LENGTH));
+    } else if(input.isJustPressed && amountCliked >= numToDisplay && currentlyDisplayingNum == false) {
+        char("b", rnd(0, G.WIDTH), rnd(0, G.LENGTH));
     }
 }
 
